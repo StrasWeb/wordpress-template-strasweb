ca1e5e989f33125b20cacac28d5702aa38475d60
Pierre Rudloff <contact@rudloff.pro>
2016-09-08 00:35:55 +0000

    Applied fixes from StyleCI

--- content-page.php
+++ content-page.php
@@ -1,24 +1,22 @@
 <?php
 /**
- * The template used for displaying page content in page.php
+ * The template used for displaying page content in page.php.
  *
- * @package WordPress
- * @subpackage Twenty_Twelve
  * @since Twenty Twelve 1.0
  */
 ?>
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<header class="entry-header">
-			<?php if ( ! is_page_template( 'page-templates/front-page.php' ) ) : ?>
+			<?php if (!is_page_template('page-templates/front-page.php')) : ?>
 			<?php the_post_thumbnail(); ?>
 			<?php endif; ?>
-			<?php if (!is_front_page() && !is_home()) : ?><h1 class="entry-title"><?php the_title(); ?></h1><?php endif;?>
+			<?php if (!is_front_page() && !is_home()) : ?><h1 class="entry-title"><?php the_title(); ?></h1><?php endif; ?>
 		</header>
 
 		<div class="entry-content">
 			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
+			<?php wp_link_pages(['before' => '<div class="page-links">'.__('Pages:', 'twentytwelve'), 'after' => '</div>']); ?>
 		</div><!-- .entry-content -->
 
 	</article><!-- #post -->
